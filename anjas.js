8337d380334ef4a625cf4f88d41c8d469d8ad8b6
chawnima <147719517+chawnima@users.noreply.github.com>
2023-12-04 14:08:37 +0700

    Add files via upload

---
+++ anjas.js
@@ -0,0 +1,59 @@
+class GraphLokasi {
+  constructor() {
+      this.graph = {};
+  }
+
+  tambahNode(start, end) {
+      if (!(start in this.graph)) {
+          this.graph[start] = [];
+      }
+      this.graph[start].push(end);
+  }
+}
+
+function dls(graph, node, goal, visited, path) {
+  visited.add(node);
+  path.push(node);
+
+  if (node === goal) {
+      return true;
+  }
+
+  for (const sekeliling of graph.graph[node] || []) {
+      if (!visited.has(sekeliling)) {
+          if (dls(graph, sekeliling, goal, visited, path)) {
+              return true;
+          }
+      }
+  }
+
+  path.pop();
+  return false;
+}
+
+function pencariRute(graph, start, goal,) {
+  const visited = new Set();
+  const path = [];
+
+  if (dls(graph, start, goal, visited, path)) {
+    document.getElementsByClassName('pathRute')[0].textContent = `Rute yang diperoleh dari ${start} ke ${goal}: ${path.join(" > ")}`;
+  } else {
+    document.getElementsByClassName('pathRute')[0].textContent = `Tidak ditemukan rute dari ${start} ke ${goal}`;
+  }
+}
+
+
+const g = new GraphLokasi();
+function tambahNode(){
+    let nodeAwal = document.getElementsByClassName('nodeAwal')[0].value;
+    let nodeTujuan = document.getElementsByClassName('nodeTujuan')[0].value;
+    g.tambahNode(nodeAwal, nodeTujuan);
+    document.getElementsByClassName('daftarNode')[0].textContent += '[' + nodeAwal + ' > ' + nodeTujuan + ']';
+}
+
+function rute(){
+    const startLandmark = document.getElementsByClassName('startLandmark')[0].value;
+    const goalLandmark = document.getElementsByClassName('goalLandmark')[0].value;
+    pencariRute(g, startLandmark, goalLandmark);
+}
+
